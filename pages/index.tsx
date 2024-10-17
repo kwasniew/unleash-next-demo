be53ff7a9e3e5502d5d14b147dec3a05eb091a98
kwasniew <kwasniewski.mateusz@gmail.com>
2024-10-17 11:31:54 +0200

    pages metrics

---
+++ pages/index.tsx
@@ -0,0 +1,33 @@
+import { evaluateFlags, flagsClient, getDefinitions } from "@unleash/nextjs";
+
+export async function getServerSideProps() {
+    const definitions = await getDefinitions();
+    const context = {};
+    const { toggles } = evaluateFlags(definitions, context);
+    const client = flagsClient(toggles);
+
+    const enabled = client.isEnabled("example-flag");
+
+    await client.sendMetrics();
+
+    return {
+        props: { enabled }, // Pass the flag value as a prop to the page
+    };
+}
+
+export default function Page({ enabled }) {
+    return (
+        <ul>
+            {enabled ? <div>ENABLED</div> : <div>DISABLED</div>}
+        </ul>
+    );
+}
+
+// setInterval(async () => {
+//     client.sendMetrics();
+// }, 5000);
+//
+// process.on("SIGTERM", async () => {
+//     await client.sendMetrics();
+//     // destroyWithFlush in node SDK
+// });
